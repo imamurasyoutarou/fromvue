9e8124e85fb7168b5d34e8c49ae2d3b53eea7b9a
imamurasyoutarou <46349073+imamurasyoutarou@users.noreply.github.com>
2019-11-18 00:27:31 +0900

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,14 +2,14 @@
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDDl3-dUlwX0A_y8-YagoIwujUmdKKaBNA",
-    authDomain: "formvue-f0bc3.firebaseapp.com",
-    databaseURL: "https://formvue-f0bc3.firebaseio.com",
-    projectId: "formvue-f0bc3",
-    storageBucket: "formvue-f0bc3.appspot.com",
-    messagingSenderId: "55498598966",
-    appId: "1:55498598966:web:c994ef4dcb8c711922c697",
-    measurementId: "G-S8ZCR093YM"
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    databaseURL: "databaseURL",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId",
+    measurementId: "measurementId"
 };
 
 
